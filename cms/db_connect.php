465e1d31ea5a170a6f04a9e27b4db0bfb76301cc
Adam Lamagna <adamjames1920@gmail.com>
2014-04-27 08:41:30 -0400

    changed db

--- cms/db_connect.php
+++ cms/db_connect.php
@@ -3,8 +3,8 @@ if ("local.thelonelama.com" === $_SERVER["SERVER_NAME"]) {
 	$db = mysql_connect("localhost", "root", "Music2981") or die("Could not connect to databse.");
 	mysql_select_db("thelonelama", $db);
 } else {
-	$db = mysql_connect("db514361520.db.1and1.com", "dbo514361520", "jul!@camp") or die("Could not connect to database");
-	mysql_select_db("db514361520", $db);
+	$db = mysql_connect("db525642704.db.1and1.com", "dbo525642704", "Music2981") or die("Could not connect to database");
+	mysql_select_db("db525642704", $db);
 }
 
 ?>
